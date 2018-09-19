0e6cf6182e580afc931d500b744f479a6f05841f
mliay81 <mikeliay@gmail.com>
2018-09-19 01:57:14 -0500

    styled

--- clickygame/src/App.js
+++ clickygame/src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import FriendCard from "./components/FriendCard";
+import Card from "./components/Card";
 import Wrapper from "./components/Wrapper";
 import Title from "./components/Title";
 import friends from "./friends.json";
@@ -11,21 +11,13 @@ class App extends Component {
     friends
   };
 
-  removeFriend = id => {
-    // Filter this.state.friends for friends with an id not equal to the id being removed
-    const friends = this.state.friends.filter(friend => friend.id !== id);
-    // Set this.state.friends equal to the new friends array
-    this.setState({ friends });
-  };
-
-  // Map over this.state.friends and render a FriendCard component for each friend object
+  // Map over this.state.friends and render a Card component for each friend object
   render() {
     return (
       <Wrapper>
-        <Title>Friends List</Title>
+        <Title>Engage!</Title>
         {this.state.friends.map(friend => (
-          <FriendCard
-            removeFriend={this.removeFriend}
+          <Card
             id={friend.id}
             key={friend.id}
             name={friend.name}
